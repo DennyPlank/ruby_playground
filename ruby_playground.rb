6baa71fa88f1f632e9f169e821a42e71e3e8e3f8
Dennis Plank <DennyMouzon@gmail.com>
2022-07-08 16:20:43 -0700

    two_sum

--- ruby_playground.rb
+++ ruby_playground.rb
@@ -1,1 +1,15 @@
-puts 'Welcome to my dojo'
+# Collection of Leet Code solves in RUBY.
+
+#  TWO_SUM
+# def two_sum(nums, target)
+#     nums.each_with_index do |num, index|
+#       val = (target - num)
+#       check = nums
+#       check[index] = nil
+#       if nums.include?(val)
+#         return [index, nums.index(val)]
+#       end
+#     end
+#   end
+
+# puts two_sum(nums, target)
