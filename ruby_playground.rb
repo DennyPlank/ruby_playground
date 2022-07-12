2eaed03de53ded71bf5584f2e6346c9fe84405cc
Dennis Plank <DennyMouzon@gmail.com>
2022-07-11 21:38:49 -0700

    couldnt finish happy number probelm. Added a merge and sort array problem

--- ruby_playground.rb
+++ ruby_playground.rb
@@ -44,3 +44,35 @@
 # end
 
 # length_of_last_word(s)
+
+# IS HAPPY (Giving up on this one for now, see 'halting' problem)
+# A happy number is a number defined by the following process:
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+
+# n = 19
+
+# def is_happy(n)
+#     n = n.digits
+#     def testing (n)
+#           n = n.digits
+#           n.map! do |item| 
+#           item**2
+#         end
+#     end
+#     testing(n)
+#     if n != 1
+#         n = n.sum
+#         testing(n)
+#     end
+#  puts "#{n} at end"
+        
+# end
+
+# is_happy(n)
+
+# Merge and sort two arrays
+def merge(nums1, m, nums2, n)
+    
+end
