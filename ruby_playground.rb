bf55531b32a54ce4ec5c10f3cbc5301b8b24cff4
Dennis Plank <DennyMouzon@gmail.com>
2022-07-24 23:46:51 -0700

    merge sort works but im not happy with it

--- ruby_playground.rb
+++ ruby_playground.rb
@@ -74,7 +74,7 @@
 
 # Merge and sort two arrays
 
-# The problem here is that this doesnt deal very well with the extra zeros
+# The problem here is that this doesnt deal very well with the extra 0's
 nums1 = [1,2,3,0,0,0]
 nums2 = [2,5,6]
 m = nums1.length
